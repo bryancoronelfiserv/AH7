8c62c83ad4be7d221e7e4acba9bc2e34d636d3e1
Bryan Coronel <bryan.coronel@fiserv.com>
2023-11-02 13:35:04 -0400

    done w ah7

--- Source.java
+++ Source.java
@@ -10,8 +10,14 @@ class Node<T> {
 
 class Source {
   public static int sumList(Node<Integer> head) {
-    // todo
-    return;
+    int sum = 0;
+    Node<Integer> cur = head;
+
+    while (cur != null) {
+      sum += cur.val;
+      cur = cur.next;
+    }
+    return sum;
   }
 
   public static void main(String[] args) {
@@ -27,8 +33,23 @@ class Source {
     d.next = e;
 
     // 2 -> 8 -> 3 -> -1 -> 7
-    
+
     System.out.println("Sum of my list: ");
     System.out.println(Source.sumList(a));
+
+    // my test case
+    // Create a new linked list for your test case
+    Node<Integer> x = new Node<>(10);
+    Node<Integer> y = new Node<>(5);
+    Node<Integer> z = new Node<>(-3);
+    Node<Integer> w = new Node<>(20);
+
+    x.next = y;
+    y.next = z;
+    z.next = w;
+
+    // Calculate the sum of the new linked list
+    System.out.println("Sum of my list: ");
+    System.out.println(Source.sumList(x)); // Output: 32
   }
 }
